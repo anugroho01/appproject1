08942690f64c062db176139502a4b4c1cf66a3ff
anugroho01 <nugrohoariska@gmail.com>
2024-01-09 11:32:22 +0700

    oosMK call API

--- lib/app/modules/oosMidik/controllers/oos_midik_controller.dart
+++ lib/app/modules/oosMidik/controllers/oos_midik_controller.dart
@@ -1,11 +1,16 @@
 import 'dart:convert';
 
+import 'package:appproject1/app/controllers/page_index_controller.dart';
 import 'package:get/get.dart';
 import 'package:http/http.dart' as http;
 
 class OosMidikController extends GetxController {
   var isLoading = false.obs;
-  OpenseaModel? openseaModel;
+  // OpenseaModel? openseaModel;
+  final pageC = Get.put(PageIndexController(), permanent: true);
+  List<getPlu> storeData = [];
+
+  // RefreshController refreshC = RefreshController();
   //TODO: Implement OosMidikController
   // RxMap<String, dynamic> body = Map<String, dynamic>().obs;
   final count = 0.obs;
@@ -17,6 +22,7 @@ class OosMidikController extends GetxController {
 
   @override
   void onReady() {
+    // getData();
     super.onReady();
   }
 
@@ -25,24 +31,82 @@ class OosMidikController extends GetxController {
     super.onClose();
   }
 
-  getData() async {
+  // getData() async {
+  //   try {
+  //     isLoading(true);
+  //     var responseData =
+  //         await http.get(Uri.parse("https://reqres.in/api/users?page=2"));
+  //     if (responseData.statusCode == 200) {
+  //       print(responseData.body);
+  //       var body = jsonDecode(responseData.body);
+  //       openseaModel = OpenseaModel.fromJson(body);
+  //     }
+  //   } catch (e) {
+  //     print('Error while getting data is $e');
+  //   } finally {
+  //     isLoading(false);
+  //   }
+  // }
+
+  Future<List<getPlu>> getData() async {
+    final apiUrl =
+        "https://oos-midikring-v1-lz7ch2oqma-et.a.run.app/oos_mk/get_plu";
     try {
       isLoading(true);
-      var responseData =
-          await http.get(Uri.parse("https://reqres.in/api/users?page=2"));
+      var responseData = await http.post(Uri.parse(apiUrl),
+          headers: {"Content-Type": "application/json"},
+          body: jsonEncode({"store_code": pageC.kd_store.value}));
       if (responseData.statusCode == 200) {
-        print(responseData.body);
+        // print(responseData.body);
+        print(pageC.kd_store.value);
         var body = jsonDecode(responseData.body);
-        openseaModel = OpenseaModel.fromJson(body);
+        // openseaModel = OpenseaModel.fromJson(body);
+        for (Map<String, dynamic> index in body) {
+          storeData.add(getPlu.fromJson(index));
+        }
+        return storeData;
+      } else {
+        print(responseData.statusCode);
+        // If the server did not return a 200 OK response,
+        // then throw an exception.
+        throw Exception('Failed to load Item');
       }
     } catch (e) {
       print('Error while getting data is $e');
+      return storeData;
     } finally {
       isLoading(false);
     }
   }
 }
 
+class getPlu {
+  String? plu;
+  String? desc;
+  int? qty;
+  // double? long;
+
+  getPlu({
+    required this.plu,
+    required this.desc,
+    required this.qty,
+    // required this.long,
+  });
+  factory getPlu.fromJson(Map<String, dynamic> json) => getPlu(
+        plu: json["plu"],
+        desc: json["name"],
+        qty: json["qty"],
+        // long: double.parse(json['address']['geo']["lng"]),
+      );
+
+  Map<String, dynamic> toJson() => {
+        "id": plu,
+        "name": desc,
+        "lat": qty,
+        // "long": long,
+      };
+}
+/*
 class OpenseaModel {
   OpenseaModel({
     required this.assets,
@@ -51,7 +115,7 @@ class OpenseaModel {
   List<Assets>? assets;
 
   OpenseaModel.fromJson(Map<String, dynamic> json) {
-    assets = List.from(json['data']).map((e) => Assets.fromJson(e)).toList();
+    assets = List.from(json[0]).map((e) => Assets.fromJson(e)).toList();
   }
 
   Map<String, dynamic> toJson() {
@@ -65,30 +129,31 @@ class Assets {
   Assets({
     required this.imageUrl,
     required this.name,
-    this.email,
+    this.plu,
     this.qty,
     // required this.permalink,
   });
 
   String? imageUrl;
   String? name;
-  String? email;
+  String? plu;
   // String? permalink;
   int? qty;
 
   Assets.fromJson(Map<String, dynamic> json) {
-    imageUrl = json['avatar'];
-    name = json['first_name'];
-    email = json['email'];
-    qty = json['id'];
+    // imageUrl = json['avatar'];
+    name = json['name'];
+    plu = json['plu'];
+    qty = json['qty'];
   }
 
   Map<String, dynamic> toJson() {
     final _data = <String, dynamic>{};
-    _data['image_url'] = imageUrl;
+    // _data['image_url'] = imageUrl;
     _data['name'] = name;
-    _data['description'] = email;
+    _data['description'] = plu;
     _data['permalink'] = qty;
     return _data;
   }
 }
+*/
--- lib/app/modules/oosMidik/views/oos_midik_view.dart
+++ lib/app/modules/oosMidik/views/oos_midik_view.dart
@@ -28,35 +28,68 @@ class OosMidikView extends GetView<OosMidikController> {
               },
               child: Text('Refresh Data'),
             ),
+            SizedBox(
+              height: 10,
+            ),
             Obx(() => pageOOS.isLoading.value
                     ? Center(
                         child: CircularProgressIndicator(),
                       )
-                    : SingleChildScrollView(
-                        scrollDirection: Axis.horizontal,
-                        child: DataTable(
-                          columns: const [
-                            DataColumn(label: Text('plu'), numeric: false),
-                            DataColumn(label: Text('desc')),
-                            DataColumn(label: Text('qty'))
+                    : Expanded(
+                        child: Column(
+                          children: [
+                            ListTile(
+                              title: Row(
+                                children: [
+                                  Expanded(
+                                      child: Text(
+                                    "PLU",
+                                    style:
+                                        TextStyle(fontWeight: FontWeight.bold),
+                                  )),
+                                  Expanded(
+                                      child: Text("Desc",
+                                          style: TextStyle(
+                                              fontWeight: FontWeight.bold))),
+                                  Expanded(
+                                      child: Text("Qty",
+                                          style: TextStyle(
+                                              fontWeight: FontWeight.bold),
+                                          textAlign: TextAlign.right)),
+                                ],
+                              ),
+                            ),
+                            Expanded(
+                              child: ListView.builder(
+                                  itemCount: pageOOS.storeData.length,
+                                  itemBuilder: (context, index) {
+                                    return ListTile(
+                                      title: Row(
+                                        children: [
+                                          Expanded(
+                                            child: Text(
+                                                pageOOS.storeData?[index].plu ??
+                                                    'no id'),
+                                          ),
+                                          Expanded(
+                                            child: Text(pageOOS
+                                                    .storeData?[index].desc ??
+                                                'no id'),
+                                          ),
+                                          Expanded(
+                                            child: Text(
+                                              pageOOS.storeData?[index].qty
+                                                      .toString() ??
+                                                  "0",
+                                              textAlign: TextAlign.right,
+                                            ),
+                                          )
+                                        ],
+                                      ),
+                                    );
+                                  }),
+                            )
                           ],
-                          rows: List.generate(
-                              pageOOS.openseaModel?.assets?.length ?? 0,
-                              (index) {
-                            return DataRow(cells: [
-                              DataCell(Text(
-                                  pageOOS.openseaModel?.assets![index].name ??
-                                      'no id')),
-                              DataCell(Text(
-                                  pageOOS.openseaModel?.assets![index].email ??
-                                      'no id')),
-                              DataCell(Text(pageOOS
-                                      .openseaModel?.assets![index].qty
-                                      .toString() ??
-                                  "0"))
-                            ]);
-                          }),
-                          showBottomBorder: true,
                         ),
                       )
                 // ListView.builder(
